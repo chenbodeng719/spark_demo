1c7844a6a4007827f776f750b2d586acdfd5abb8
chenbodeng <chenbodeng@hireez.com>
2022-12-27 04:07:35 +0000

    add setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # sudo su
-sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hsconn.zip /home/hadoop/
-sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hbase_jar.zip /home/hadoop/
-# sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hbase-site.xml /usr/lib/spark/conf/
+sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hsconn.zip /home/emr_tool/
+sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hbase_jar.zip /home/emr_tool/
+sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hbase-site.xml /home/emr_tool/
 # aws s3 cp s3://htm-test/chenbodeng/mytest/hadoop_jars.zip ./
 # sudo aws s3 cp s3://htm-test/chenbodeng/mytest/spark_jars.zip /home/hadoop/
-cd /home/hadoop
+cd /home/emr_tool
 sudo unzip hsconn.zip
 sudo unzip hbase_jar.zip
 
