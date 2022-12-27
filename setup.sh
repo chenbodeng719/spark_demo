db066de2f0c883055c9a649c398a1a13f741579b
chenbodeng <chenbodeng@hireez.com>
2022-12-27 03:11:51 +0000

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-pip3 install peewee
+#!/bin/bash
+# sudo su
+sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hsconn.zip /home/hadoop/
+sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hbase_jar.zip /home/hadoop/
+sudo aws s3 cp s3://htm-test/chenbodeng/mytest/hbase-site.xml /usr/lib/spark/conf/
+# aws s3 cp s3://htm-test/chenbodeng/mytest/hadoop_jars.zip ./
+# sudo aws s3 cp s3://htm-test/chenbodeng/mytest/spark_jars.zip /home/hadoop/
+cd /home/hadoop
+sudo unzip hsconn.zip
+sudo unzip hbase_jar.zip
+
+# sudo pip3 install boto3 scikit-learn
--- setup_prod.sh
+++ setup_prod.sh
@@ -5,6 +5,7 @@ sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hbase_jar.zip /home/hadoo
 sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hbase-site.xml /usr/lib/spark/conf/
 # aws s3 cp hsconn.zip s3://hiretual-ml-data-test/cbd/emr_tool/
 # aws s3 cp hbase_jar.zip s3://hiretual-ml-data-test/cbd/emr_tool/
+# aws s3 cp hbase-site.xml s3://hiretual-ml-data-test/cbd/emr_tool/
 cd /home/hadoop
 sudo unzip hsconn.zip
 sudo unzip hbase_jar.zip
