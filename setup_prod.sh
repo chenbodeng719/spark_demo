7fc27a66718b3b17d88e9c26adf12d18165fb229
chenbodeng <chenbodeng@hireez.com>
2023-01-04 09:15:37 +0000

    add prod shell

--- setup_prod.sh
+++ setup_prod.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # sudo su
-sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hsconn.zip /home/hadoop/
-sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hbase_jar.zip /home/hadoop/
-sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hbase-site.xml /usr/lib/spark/conf/
+sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hsconn.zip /home/emr_tool/
+sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hbase_jar.zip /home/emr_tool/
+sudo aws s3 cp s3://hiretual-ml-data-test/cbd/emr_tool/hbase-site.xml /home/emr_tool/
 # aws s3 cp hsconn.zip s3://hiretual-ml-data-test/cbd/emr_tool/
 # aws s3 cp hbase_jar.zip s3://hiretual-ml-data-test/cbd/emr_tool/
 # aws s3 cp hbase-site.xml s3://hiretual-ml-data-test/cbd/emr_tool/
-cd /home/hadoop
+cd /home/emr_tool
 sudo unzip hsconn.zip
 sudo unzip hbase_jar.zip
+
+sudo pip3 install boto3 scikit-learn
