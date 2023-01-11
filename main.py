3fdcfb2255190fb748f10d20aa32488fb091c219
chenbodeng <chenbodeng@hireez.com>
2023-01-11 08:09:16 +0000

    add hbase mget

--- main.py
+++ main.py
@@ -97,6 +97,40 @@ def test_hbase_count():
     # )
     print(df.count())
 
+def test_hbase_mget():
+    spark = SparkSession.builder.appName("test_hbase_mget").getOrCreate()
+    sc = spark.sparkContext
+    sqlc = SQLContext(sc)
+    data_source_format = 'org.apache.hadoop.hbase.spark'
+    tname = "candidate"
+    tmap = "uid STRING :key, oridata STRING f1:data"
+    df = sqlc.read.format(data_source_format) \
+        .option('hbase.table',tname) \
+        .option('hbase.columns.mapping', tmap) \
+        .option('hbase.spark.use.hbasecontext', False) \
+        .option("hbase.spark.pushdown.columnfilter", False) \
+        .load()
+    # df = df \
+    # .withColumn("position_title",get_json_object(col("oridata"), "$.basic.current_position.position_title") ) \
+    # .filter(col("position_title") == "Business Consultant") \
+    # .select(
+    #     "uid",
+    #     "position_title",
+    #     "oridata",
+    # )
+    tlist = ["fcon_59124a3873d8f0.85770051", "tiq_58d1937c3a8550.54523267", "impt_5b53011b7d6a83.61364387", "impt_5b5b93e4c66628.23200830", "fu592c680b53c8f5.48096564", "fu589264bb564313.87757324", "fu589b42474b6013.97777442", "fu58aae034c0af85.13465164", "fu588b70d713aef5.64979322", "impt_5b695d2eeccb03.71995693"]
+#     .filter(col("position_title") == "Business Consultant") \
+    df = df \
+    .filter(df.uid.isin(tlist)) \
+    .withColumn("position_title",get_json_object(col("oridata"), "$.basic.current_position.position_title") ) \
+    .select(
+        "uid",
+        "position_title",
+        "oridata",
+    )
+                  
+    df.show()
+
 
 def merge_backlog(runenv):
     spark = SparkSession.builder.appName("merge_backlog").getOrCreate()
@@ -143,6 +177,8 @@ if __name__ == "__main__":
     #     test_spark_hbase(spark)
     if cate == "test_hbase_count":
         test_hbase_count()
+    elif cate == "test_hbase_mget":
+        test_hbase_mget()
     elif cate == "test_hbase":
         test_hbase()
     elif cate == "merge_backlog":
